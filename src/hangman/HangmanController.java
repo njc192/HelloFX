8683caf2764d129bfdb5d4ecbad6b2e5c2925298
njc19 <njc192@gmail.com>
2021-05-28 16:23:07 -0700

    added blue for win and red for loss

--- src/hangman/HangmanController.java
+++ src/hangman/HangmanController.java
@@ -14,6 +14,7 @@ import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 
 import java.net.URL;
+import java.util.Locale;
 import java.util.ResourceBundle;
 
 public class HangmanController implements Initializable {
@@ -38,23 +39,69 @@ public class HangmanController implements Initializable {
 
 
 
+    private Node [] hangmanParts;
     private String word;
+    private int wrong_count;
+    private int correct_count;
 
     public HangmanController(String word)
     {
         this.word = word;
+        System.out.println(word);
+        wrong_count = 0;
+        correct_count =0;
+        hangmanParts = new Node[7];
 
     }
 
     public void guess(ActionEvent event)
     {
+
+
         if (event.getSource() instanceof Button)
         {
             Button b = (Button) event.getSource();
-
-            System.out.println(b.getText());
-
             b.setDisable(true);
+            char letter = b.getText().toLowerCase(Locale.ROOT).charAt(0);
+            if (word.contains(""+letter))
+            {
+                for (int i = 0; i < word.length(); i++)
+                {
+                    if (word.charAt(i) == letter)
+                    {
+                        ((Text)((StackPane)word_box.getChildren().get(i)).getChildren().get(0)).setText(""+letter);
+                        correct_count++;
+                    }
+                }
+            }
+            else
+            {
+                hangmanParts[wrong_count++].setVisible(true);
+            }
+
+            if (correct_count == word.length())
+            {
+                System.out.println("win");
+                for (int i = 0; i < word.length(); i++)
+                {
+                    Text t = ((Text)((StackPane)word_box.getChildren().get(i)).getChildren().get(0));
+                    t.setFill(Color.BLUE);
+                    t.setText(""+word.charAt(i));
+                }
+            }
+
+            if (wrong_count > hangmanParts.length -1 )
+            {
+                System.out.println("LOST");
+                for (int i = 0; i < word.length(); i++)
+                {
+                    Text t = ((Text)((StackPane)word_box.getChildren().get(i)).getChildren().get(0));
+                    t.setFill(Color.RED);
+                    t.setText(""+word.charAt(i));
+                }
+
+            }
+
         }
     }
 
@@ -71,20 +118,20 @@ public class HangmanController implements Initializable {
             word_box.getChildren().add(pane);
         }
 
-//        Node[] hangmanParts = new Node[7];
-//        hangmanParts[0] = hangman_rope;
-//        hangmanParts[1] = hangman_head;
-//        hangmanParts[2] = hangman_body;
-//        hangmanParts[3] = hangman_left_arm;
-//        hangmanParts[4] = hangman_right_arm;
-//        hangmanParts[5] = hangman_right_leg;
-//        hangmanParts[6] = hangman_left_leg;
-//
-//
-//        for (int i = 0; i < hangmanParts.length; i++)
-//        {
-//                hangmanParts[i].setVisible(false);
-//        }
+
+        hangmanParts[0] = hangman_rope;
+        hangmanParts[1] = hangman_head;
+        hangmanParts[2] = hangman_body;
+        hangmanParts[3] = hangman_left_arm;
+        hangmanParts[4] = hangman_right_arm;
+        hangmanParts[5] = hangman_right_leg;
+        hangmanParts[6] = hangman_left_leg;
+
+
+        for (int i = 0; i < hangmanParts.length; i++)
+        {
+            hangmanParts[i].setVisible(false);
+        }
 
     }
 }
